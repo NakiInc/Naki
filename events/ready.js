12202963e239748da855298b8d26044ee6884594
Z-BotInc <62592978+Z-BotInc@users.noreply.github.com>
2020-04-15 18:52:36 +0200

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -4,9 +4,9 @@ const {Client} = require('discord.js');
  * ╔═════════════════════════════════════════════════════════════════╗
  * ║  Événement ready du bot                                         ║                             
  * ║═════════════════════════════════════════════════════════════════║                                                                
- * ║  @param {Client} bot                                            ║
- * ╚═════════════════════════════════════════════════════════════════╝
+ * @param {Client} bot
+ *
  */
 module.exports = async (bot) => {
-
+    bot.user.setActivity(`${bot.guilds.cache.size} serveur(s) | ${bot.config.prefix}help.`, { type: "WATCHING" });
 };
